64f3a9f0846463d0f34f5dbf20af99de6efc1baa
Agung Ramadhan Suyono <agungramadhans.work@gmail.com>
2024-05-09 03:21:00 +0700

    6. Save to Database and check DB using Prisma

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,15 +3,56 @@ import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
 	const { username, email, password } = req.body;
-   
-   //HASH THE PASSWORD
-   const hashedPassword = await bcrypt.hash(password, 10);
-   console.log(hashedPassword);
-	
+
+	try {
+		//HASH THE PASSWORD
+		const hashedPassword = await bcrypt.hash(password, 10);
+		console.log(hashedPassword);
+
+		// CREATE A NEW USER AND SAVE TO DB
+		const newUser = await prisma.user.create({
+			data: {
+				username,
+				email,
+				password: hashedPassword,
+			},
+		});
+
+		console.log(newUser);
+
+		res.status(201).json({ message: "User created successfully" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Failed to create user" });
+	}
 };
 
-export const login = (req, res) => {
-	//db operations
+export const login = async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		// CHECK IF THE USER EXIST
+		const user = await prisma.user.findUnique({
+			where: { username },
+		});
+
+		if (!user)
+			return res.status(401).json({ message: "Invalid credentials" });
+
+		const isValidPassword = await bcrypt.compare(password, user.password);
+
+		if (!isValidPassword)
+			return res.status(401).json({ message: "Invalid credentials" });
+
+		// CHECK IF THE PASSWORD IS CORRECT
+
+      
+
+		// GENERATE COOKIE TOKEN AND SEND TO THE USER
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Failed to login" });
+	}
 };
 
 export const logout = (req, res) => {
