b979cefd76a1c9a700d96bc3c11c9e8c3287a088
Agung Ramadhan Suyono <agungramadhans.work@gmail.com>
2024-05-12 07:30:37 +0700

    9. Middleware

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,14 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const shouldBeLoggedIn = async (req, res) => {
-   const token = req.cookies.token;
-
-   if(!token) return res.status(401).json({message: "Not authenticated!"});
-
-   jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
-      if (err) return res.status(403).json({message: "Token is not valid!"});
-   })
-
+   console.log(req.userId);
    res.status(200).json({message: "You are authenticated :)"});
 }
 
